939426c7797e66029582163ade886196adb0567b
Parthkhunt27 <khuntparth2003@gmail.com>
2024-05-15 11:24:02 +0530

    first commit

---
+++ 3-myntra-react-clone/src/components/Homeitem.jsx
@@ -0,0 +1,52 @@
+import { useDispatch, useSelector } from "react-redux";
+import { bagActions } from "../store/bagSlice";
+import { IoAddCircle } from "react-icons/io5";
+import { CiCircleRemove } from "react-icons/ci";
+
+const Homeitem = ({ item }) => {
+  const dispatch = useDispatch();
+  const bagItems = useSelector((store) => store.bag);
+  const elementFound = bagItems.indexOf(item.id) >= 0;
+
+  const handleAddToBag = () => {
+    dispatch(bagActions.addToBag(item.id));
+  };
+  const handleRemove = () => {
+    dispatch(bagActions.removeFromBag(item.id));
+  };
+  return (
+    <div className="item-container">
+      <img className="item-image" src={item.image} alt="item image" />
+      <div className="rating">
+        {item.rating.stars} ⭐ | {item.rating.count}
+      </div>
+      <div className="company-name">{item.company}</div>
+      <div className="item-name">{item.item_name}</div>
+      <div className="price">
+        <span className="current-price">Rs {item.current_price}</span>
+        <span className="original-price">Rs {item.original_price}</span>
+        <span className="discount">({item.discount_percentage}% OFF)</span>
+      </div>
+
+      {elementFound ? (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-danger"
+          onClick={handleRemove}
+        >
+          <CiCircleRemove /> Remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-success"
+          onClick={handleAddToBag}
+        >
+          <IoAddCircle /> Add to Bag
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Homeitem;
---
+++ 3-myntra-react-clone/src/routes/Home.jsx
@@ -0,0 +1,17 @@
+import Homeitem from "../components/Homeitem";
+import { useSelector } from "react-redux";
+const Home = () => {
+  const items = useSelector((store) => store.items);
+  console.log(items);
+  return (
+    <main>
+      <div className="items-container">
+        {items.map((item) => (
+          <Homeitem key={item.id} item={item} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
